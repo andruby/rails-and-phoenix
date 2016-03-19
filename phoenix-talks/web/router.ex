fb134c8bc7ad180ac2df6f9c57c45aab175672e3
Andrew Fecheyr <andrew@bedesign.be>
2016-03-19 23:55:58 +0100

    [phoenix] mix phoenix.gen.html Talk talks title presenter

--- phoenix-talks/web/router.ex
+++ phoenix-talks/web/router.ex
@@ -17,6 +17,7 @@ defmodule Talks.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/talks", TalkController
   end
 
   # Other scopes may use custom stacks.
